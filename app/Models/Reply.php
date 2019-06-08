b0e3a521785f57d4c0349ea7e319b7a718d2141e
Brandon A <babbassp@umflint.edu>
2019-06-08 01:51:43 -0400

    Rename threads test, add eloquent relationships for threads and replies

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
 
+    /**
+     * The user associated with the reply.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The thread associated with the reply.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -4,7 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Thread
+ *
+ * @package App\Models
+ *
+ * @property int                                             $id
+ * @property int                                             $user_id
+ * @property string                                          $title
+ * @property string                                          $body
+ * @property \Illuminate\Database\Eloquent\Relations\HasMany $replies
+ */
 class Thread extends Model
 {
 
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * The replies associated with a thread.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
