88f7dea46b553e1be4757c1132b223e4fe0a3a92
Brandon A <babbassp@umflint.edu>
2019-06-17 14:34:53 -0400

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,38 @@
 |
 */
 
-Route::resource('threads', 'ThreadsController');
-Route::post('threads/{thread}/replies', 'RepliesController@store')
+
+///////////////// Channel ///////////////////////
+//Route::get('threads/{channel}', [
+//    'as'   => 'threads.channel.index',
+//    'uses' => 'ChannelController@index'
+//]);
+
+///////////////// Threads ///////////////////////
+//Route::get('threads/{channel}/create', [
+//    'as'   => 'threads.create',
+//    'uses' => 'ThreadsController@create'
+//]);
+//Route::post('threads/{channel}', [
+//    'as'   => 'threads.store',
+//    'uses' => 'ThreadsController@store'
+//]);
+Route::get('threads/{channel}/{thread}', [
+    'as'   => 'threads.show',
+    'uses' => 'ThreadsController@show'
+]);
+Route::resource('threads', 'ThreadsController', [
+    'except' => ['show']
+]);
+
+/////////////// Replies /////////////////////////
+Route::get('threads/{channel}/{thread}/replies', [
+    'as'  => 'threads.replies',
+    'use' => 'RepliesController@show'
+]);
+Route::post('threads/{channel}/{thread}/replies', 'RepliesController@store')
     ->name('threads.reply.store');
 
+////////////// Authentication //////////////////
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
