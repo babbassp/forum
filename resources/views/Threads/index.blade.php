191c12aaf81abae5728100b18ea307c1bef16cd2
Brandon A <babbassp@umflint.edu>
2019-07-16 21:50:00 -0400

    Move each thread to own panel

--- resources/views/Threads/index.blade.php
+++ resources/views/Threads/index.blade.php
@@ -4,28 +4,25 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10">
-                <div class="card">
-                    <div class="card-header">Forum Threads</div>
-                    <div class="card-body">
-                        @foreach($threads as $thread)
-                            <article>
-                                <div class="card-title">
-                                    <div class="d-flex justify-content-between">
-                                        <div class="p-2">
-                                            <h4><a href="{{ route('threads.show', $thread->getUrlParams()) }}">
-                                                    {{ $thread->title }}</a></h4>
-                                        </div>
-                                        <div class="p-2">
-                                            {{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}
-                                        </div>
-                                    </div>
+                @foreach($threads as $thread)
+                    <div class="card mb-3">
+                        <div class="card-header">
+                            <div class="d-flex justify-content-between">
+                                <div class="p-2">
+                                    <h4><a href="{{ route('threads.show', $thread->getUrlParams()) }}">
+                                            {{ $thread->title }}
+                                        </a></h4>
                                 </div>
-                                {{ $thread->body }}
-                            </article>
-                            <hr>
-                        @endforeach
+                                <div class="p-2">
+                                    {{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            {{ $thread->body }}
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,25 +2,27 @@
 
 @section('content')
     <div class="container">
-        <div class="page-item">
-            <h1>
-                {{ $profileUser->name }}
-            </h1>
+        <div class="text-center mb-3">
+            <h1 class="text-white">{{ $profileUser->name }}</h1>
         </div>
         @foreach($threads as $thread)
-            <div class="card mb-3">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <a class="card-link"
-                           href="{{ route('threads.show', $thread->getUrlParams()) }}">
-                            {{ $thread->title }}
-                        </a>
-                        <span>{{$thread->created_at->diffForHumans()}}</span>
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <a class="card-link"
+                                   href="{{ route('threads.show', $thread->getUrlParams()) }}">
+                                    {{ $thread->title }}
+                                </a>
+                                <span>{{$thread->created_at->diffForHumans()}}</span>
+                            </div>
+                            <hr>
+                            <p class="card-text">
+                                {{ $thread->body }}
+                            </p>
+                        </div>
                     </div>
-                    <hr>
-                    <p class="card-text">
-                        {{ $thread->body }}
-                    </p>
                 </div>
             </div>
         @endforeach
