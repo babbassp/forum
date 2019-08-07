6ee566467e67aaded87764421a8654ed54c0c64f
Brandon A <babbassp@umflint.edu>
2019-08-07 19:44:08 -0400

    Add some factories for favorites

--- database/factories/FavoriteFactory.php
+++ database/factories/FavoriteFactory.php
@@ -3,6 +3,7 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\Models\Favorite;
+use App\Models\Reply;
 use App\Models\User;
 use Faker\Generator as Faker;
 
@@ -13,5 +14,17 @@ $factory->define(Favorite::class, function (Faker $faker) {
         },
         'favorited_id'   => null,
         'favorited_type' => null,
+    ];
+});
+
+$factory->state(Favorite::class, 'reply', function ($faker) {
+    return [
+        'user_id'        => function () {
+            return factory(User::class)->create()->id;
+        },
+        'favorited_id'   => function () {
+            return factory(Reply::class)->create()->id;
+        },
+        'favorited_type' => Reply::class,
     ];
 });
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Models\Favorite;
+use App\Models\Reply;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
@@ -22,7 +24,27 @@ $factory->define(User::class, function (Faker $faker) {
         'name'              => $faker->name,
         'email'             => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         'remember_token'    => Str::random(10),
     ];
+});
+
+$factory->state(User::class, 'favorited_reply', function ($faker) {
+    return [
+        'name'              => $faker->name,
+        'email'             => $faker->unique()->safeEmail,
+        'email_verified_at' => now(),
+        'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        'remember_token'    => Str::random(10),
+    ];
+});
+
+$factory->afterCreatingState(User::class, 'favorited_reply', function ($user, $faker) {
+    $reply = factory(Reply::class)->create();
+    factory(Favorite::class)->create([
+        'user_id'        => $user->id,
+        'favorited_id'   => $reply->id,
+        'favorited_type' => get_class($reply)
+    ]);
+    $user->replies()->save($reply);
 });
