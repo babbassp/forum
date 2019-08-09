1470a33439bbe29d54e1a819acdebac2a8d1cd17
Brandon A <babbassp@umflint.edu>
2019-08-09 01:44:42 -0400

    If unauthenticated user visits home redirect to login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
-
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -23,12 +11,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if (! Auth::check()) {
-            return view('home');
+        if (auth()->guest()) {
+            return view('auth.login');
         }
 
         return redirect(
-            route('profile', Auth::user())
+            route('profile', auth()->user())
         );
     }
 }
